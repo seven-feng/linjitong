568bc31ad6b025ae999e9ff0b4d98b68c5886754
unknown <zf7067@HKGY170128.hikyjy.com.cn>
2019-04-24 14:43:11 +0800

    190424 g

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"https://www.easy-mock.com/mock/5c124a537a9b075d57f87ce0/ljt"'
+  // BASE_API: '"https://www.easy-mock.com/mock/5c124a537a9b075d57f87ce0/ljt"'
+  BASE_API: '"/TDS"'
 })
