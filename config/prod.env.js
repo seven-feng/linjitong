f760baf415e48105cecef6a49befc12a22ef2f2a
zhangfeng <zhangfeng@cetiti.com>
2019-07-08 09:22:27 +0800

    20190708

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  // BASE_API: '"http://10.70.1.117:8081/TDS"'
-  BASE_API: '"http://114.55.114.93:8080/TDS"'
+  BASE_API: '"http://218.205.117.90/TDS"'
 }
